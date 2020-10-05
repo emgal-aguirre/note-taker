b4bfb08f15e8f83c896540cd673329472a51d195
Emily Aguirre <emgal.aguirre@gmail.com>
2020-10-05 13:22:58 -0500

    struggling to get it to post

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ app.get("/api/notes", function (req, res) {
 
 // Create new POST 
 app.post("/api/notes", function (req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("/db/db.json"));
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNote = req.body;
     let uniqueID = (savedNotes.length).toString();
     newNote.id = uniqueID;
     savedNotes.push(newNote);
 
-    fs.writeFileSync(/db/db.JSON.stringify(savedNotes));
+    fs.writeFileSync("/db/db.json".JSON.stringify(savedNotes));
     console.log("Note saved to db.json. Content: ", newNote);
     res.JSON(savedNotes);
 })
