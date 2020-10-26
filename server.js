a66304c75b3bd1aa39829dda8cdcb91fadb16ea8
Emily Aguirre <emgal.aguirre@gmail.com>
2020-10-26 15:28:26 -0500

    corrected syntax errors

--- server.js
+++ server.js
@@ -1,56 +1,72 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
+const uuid = require("uuid");
+
+const notesArr = require("./db/db.json");
+
+
 
 // Sets up the Express App
 // =============================================================
 
-var app = express();
-var PORT = 8000;
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 // HTML Routes
 // =============================================================
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-// Dispay with API/ 
 
 app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
-
-// Create new POST 
-app.post("/api/notes", function (req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let newNote = req.body;
-    let uniqueID = (savedNotes.length).toString();
-    newNote.id = uniqueID;
-    savedNotes.push(newNote);
-
-    fs.writeFileSync("/db/db.json".JSON.stringify(savedNotes));
-    console.log("Note saved to db.json. Content: ", newNote);
-    res.JSON(savedNotes);
+    res.json(notesArr);
 })
+
+app.post("/api/notes", function (req, res) {
+    const addNote = {
+        id: uuid.v4(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    notesArr.push(addNote);
+    res.json(notesArr);
+});
+// app.delete("/api/notes/:id", function (req, res) {
+//     let noteId = req.params.id;
+
+//     for (let i = 0; i < notesArr.length; i++) {
+//         if (noteId === notesArr[i].id) {
+//             notesArr.splice(i, 1);
+//             res.json(notesArr);
+//         };
+//     };
+// });
+
+app.delete("/api/notes/:id", function (req, res) {
+    let noteId = req.params.id;
+
+    for (let i = 0; i < notesArr.length; i++) {
+        if (noteId == notesArr[i].id) {
+            notesArr.splice(i, 1);
+            res.json(notesArr);
+        };
+    };
+});
 
 
 // Starts the server to begin listening
 // =============================================================
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+app.listen(PORT, () => {
+    console.log("App is listening on PORT: " + PORT);
 });
